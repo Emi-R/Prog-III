dae1f59fcdb2f5c5c3c456d187d93bc69fac6f37
Emi-R <88684755+Emi-R@users.noreply.github.com>
2022-09-07 14:06:51 -0300

    Modificacion DB y Scripts

--- ScriptTablas.sql
+++ ScriptTablas.sql
@@ -4,12 +4,17 @@ Go
 Use AplicacionPersonas
 Go
 
+Create Table EstadoCivil_X_ID(
+	ID Tinyint Primary Key,
+	Estado Varchar(10) Not Null,
+)
+
 Create Table Personas(
 	DNI Varchar(50) Not Null Primary Key,
 	Apellido Varchar(50) Not Null,
 	Nombres Varchar(50) Not Null,
 	FechaNac Date Not Null,
-	EstadoCivil Tinyint Not Null,
-	Sexo Bit Not Null,
-
+	EstadoCivil Tinyint Not Null Foreign Key References EstadoCivil_X_ID(ID),
+	Sexo Varchar(1) Null,
 )
+Go
